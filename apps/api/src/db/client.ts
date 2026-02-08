43a15e3eb374be9e850983a1bcd30856a903580b
Wesley Bliss <wesley.bliss@gmail.com>
2026-02-08 00:41:06 -0500

    Strict types

--- apps/api/src/db/client.ts
+++ apps/api/src/db/client.ts
@@ -1,12 +1,10 @@
 import tursoDatabase from '@/db/turso/client.turso'
 import postgresDatabase from '@/db/postgres/client.postgres'
-import { NeonHttpDatabase } from 'drizzle-orm/neon-http'
-import { LibSQLDatabase } from 'drizzle-orm/libsql'
 
 if (!process.env.DATABASE_DIALECT)
     throw new Error('client.ts: DATABASE_DIALECT env variable is not set')
 
-type SupportedDatabase = any
+type SupportedDatabase = typeof tursoDatabase | typeof postgresDatabase
 
 export const db: SupportedDatabase = process.env.DATABASE_DIALECT === 'turso'
     ? tursoDatabase!
